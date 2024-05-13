7b1a4f7f4b188b295138cddb5f22409cc0c6bdd4
quanghuong <quanghuong.dev@gmail.com>
2024-05-13 12:06:50 +0700

    setup nodejs env

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const app = require('./src/app');
+const ENV = require('./config/index');
+const connectorInstance = require('./src/database/connector.database');
+
+const APP = ENV.APP;
+const server = app.listen(ENV.APP.PORT, () => {
+    console.log('====================================');
+    console.log(
+        `...Listening on ${APP.PROTOCOL}://${APP.DOMAIN}:${APP.PORT}`
+    );
+    console.log('====================================');
+});
+
+/* Press Ctrl + C to close the server */
+process.on('SIGINT', async () => {
+    /* Close the server */
+    server.close((error) => {
+        if (error)
+            console.log('Encouter problems from closing the server');
+
+        console.log(
+            `Closed the server on ${APP.PROTOCOL}://${APP.DOMAIN}:${APP.PORT}`
+        );
+    });
+});
